1c5d4206279f15ab9dff4ec8105df3ce6c4332f2
lo-b <bram_lo@outlook.com>
2025-09-13 20:55:58 +0200

    wip(module_01): add debuging exercise and expand chaining

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from langchain_core.globals import set_debug
 
-from module_01.chaining import adder_chain, parallel_chain, s
+from module_01.chaining import (
+    adder_chain,
+    parallel_chain,
+    parallel_double_surround,
+    s,
+    surround_thrice,
+)
 from module_01.runnables import add_one, adder, greeter, lambda_adder
 
 
 def main():
     print("==== module_01 ====")
-    set_debug(True)
     print("Add one to 7:", add_one.invoke(7))
-    set_debug(False)
     print("Adding 3 and 9:", adder.invoke({"a": 3, "b": 9}))
     print("Greeter:", greeter.invoke("World!"))
     print(f"Lambda adder: {lambda_adder.invoke({'a': 1, 'b': 2})}")
-
     print(f"Adder chain: {adder_chain.invoke({'a': 1, 'b': 2})}")
+
+    print("PARALLEL CHAIN")
+    print(parallel_chain.get_graph().draw_ascii())
+    set_debug(True)
     print(f"parallel adder: {parallel_chain.invoke({'a': 7, 'b': 2})}")
+    set_debug(False)
     print(s.get_graph().draw_ascii())
     print(s.invoke({"a": 2, "b": 4}))
+
+    print(surround_thrice.invoke(3))
+    print(parallel_double_surround.invoke({"a": 3, "b": 3}))
 
 
 if __name__ == "__main__":
--- module_01/chaining.py
+++ module_01/chaining.py
@@ -1,4 +1,9 @@
-from langchain_core.runnables import RunnableLambda, RunnableParallel, RunnableSequence
+from langchain_core.runnables import (
+    RunnableLambda,
+    RunnableParallel,
+    RunnablePassthrough,
+    RunnableSequence,
+)
 
 from .runnables import adder
 
@@ -10,5 +15,35 @@ _t: RunnableLambda[int, dict[str, int]] = RunnableLambda(
 )
 
 adder_chain = RunnableSequence(adder, _t, adder)
+
+# TODO: create a sequential chain of adders (two in- and outputs)
 parallel_chain = RunnableParallel(a=adder, b=adder)
+
+# TODO: create a sequential chain with the following signature:
+# f(a, b) -> c, and the following chain:
+#   1. run the adder in parallel
+#   2. run the adder, again, on the output of (1)
+# INFO: build 'inside-out'; first build the parallel chain;
+# then chain this again (as a sequence)
 s = RunnableSequence(parallel_chain, adder)
+
+
+# TODO: composing of runnables is also possible using LangChain Expression Language (LCEL)
+# Compose a simple chain that adds parenthesis '()' around its input.
+def _f(x):
+    return f"({x})"
+
+
+surround = RunnableLambda(_f)
+
+surround_thrice = surround | surround | surround
+
+# TODO: Create a parallel chain using LCEL
+# (note that '|' can only be used to create a RunnableSequence from two runnables,
+# I.e. dictionary is not a RunnableParallel object)
+# LCEL with parallel
+# INFO: (hint) use 'RunnablePassthrough'
+parallel_double_surround = RunnablePassthrough() | {
+    "a": surround | surround,
+    "b": surround | surround,
+}
