9d4672d8a33a76fe99f69f744a4afbd98a446648
lo-b <bram_lo@outlook.com>
2025-09-19 02:50:24 +0200

    feat(module_04): split input, output and overall state in prompt chain worfklow

--- module_04/src/agent/graph.py
+++ module_04/src/agent/graph.py
@@ -4,7 +4,6 @@ Returns a predefined response. Replace logic and configuration as needed.
 """
 
 import asyncio
-from dataclasses import dataclass
 from typing import Any, Dict, TypedDict
 
 from langchain_anthropic import ChatAnthropic
@@ -23,20 +22,20 @@ class Context(TypedDict):
     my_configurable_param: str
 
 
-@dataclass
-class State:
-    """Input state for the agent.
-
-    Defines the initial structure of incoming data.
-    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
-    """
-
+# INFO: define separate input, output and overall states; nicer UX.
+class InputState(TypedDict):
     vacancy: str
-    summary: str = ""
-    draft: str = ""
 
 
-async def summarize(state: State, runtime: Runtime[Context]) -> Dict[str, Any]:
+class OutputState(TypedDict):
+    response: str
+
+
+class OverallState(InputState, OutputState):
+    pass
+
+
+async def summarize(state: InputState, runtime: Runtime[Context]) -> Dict[str, Any]:
     """Process input and returns output.
 
     Can use runtime context to alter behavior.
@@ -48,16 +47,21 @@ async def summarize(state: State, runtime: Runtime[Context]) -> Dict[str, Any]:
     chain = prompt | model
 
     try:
-        summary = await chain.ainvoke({"vacancy_description": state.vacancy})
+        summary = await chain.ainvoke({"vacancy_description": state["vacancy"]})
     except Exception as e:
         print(f"Error during summarization: {e}")
         summary = "Failed to generate summary"
 
-    return {"summary": summary}
+    return {"vacancy": state["vacancy"], "response": summary}
 
 
 graph = (
-    StateGraph(State, context_schema=Context)
+    StateGraph(
+        OverallState,
+        input_schema=InputState,
+        output_schema=OutputState,
+        context_schema=Context,
+    )
     .add_node(summarize)
     .add_edge("__start__", "summarize")
     .compile(name="New Graph")
