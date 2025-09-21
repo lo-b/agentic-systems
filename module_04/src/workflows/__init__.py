98df9af7d45863fa34449fcbfc150b66a2376ac7
lo-b <bram_lo@outlook.com>
2025-09-22 00:29:19 +0200

    wip(chained_routing): add working azure llm prompt chain + route

--- module_04/src/workflows/__init__.py
+++ module_04/src/workflows/__init__.py
@@ -1,6 +1,7 @@
 # FIX: Running main.py file in root dir might raise 'ModuleNotFoundError'.
 # Can be 'fixed' by prefixing module with a '.', e.g. '.basic_chatbot'
 from basic_chatbot import basic_chatbot
+from chained_routing import chained_routing
 from prompt_chaining import prompt_chain
 from simple_routing import simple_routing
 
@@ -8,4 +9,5 @@ __all__ = [
     "basic_chatbot",
     "prompt_chain",
     "simple_routing",
+    "chained_routing",
 ]
