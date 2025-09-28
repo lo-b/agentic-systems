612f456f1abdbbab711a8555131f1c5ec177660b
lo-b <bram_lo@outlook.com>
2025-09-29 00:13:05 +0200

    feat(module_01): remove token exercise solution

--- module_01/tokens.py
+++ module_01/tokens.py
@@ -1,13 +1,6 @@
 import tiktoken
 
-# TODO: use the tiktoken encoder to the string 'Hello agentic world!'
-# Q: Taking the 'gpt-4' model, how many tokens does the string consist of?
-s = "Hello agentic world!"
 
-
-def print_tokens(s: str) -> list[str]:
-    enc = tiktoken.encoding_for_model("gpt-4")
-    texts = enc.encode(s)
-    tokens = enc.decode_tokens_bytes(texts)
-
-    return [token.decode() for token in tokens]
+# TODO: Exercise 01
+def decode_tokens():
+    raise NotImplementedError()
