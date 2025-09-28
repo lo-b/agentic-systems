afb64b03064920aa83a5994d4a48e245ac706fbb
lo-b <bram_lo@outlook.com>
2025-09-28 19:34:41 +0200

    feat(module_01): remove runnable exercise solutions

---
+++ module_01/main.py
@@ -0,0 +1,11 @@
+# INFO: a module (python script) its functions can be imported:
+from runnables import greet
+
+
+def main():
+    print("==== module_01 ====")
+    print(greet("Agentic World!"))
+
+
+if __name__ == "__main__":
+    main()
