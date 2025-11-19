9d0a87990a81536df12fea4556008554e6c84689
lo-b <bram_lo@outlook.com>
2025-11-19 21:55:55 +0100

    feat(module_01): remove solution; add starter

--- module_01/main.py
+++ module_01/main.py
@@ -1,52 +1,14 @@
-import math
+from dotenv import load_dotenv
+from runnables import greet
 
-from chaining import (
-    adder_chain,
-    parallel_chain,
-    parallel_double_surround,
-    sequence,
-    surround_thrice,
-)
-from document import file_runnable
-from langchain_core.globals import set_debug
-from runnables import add_one_runnable, adder_runnable, greeter_runnable, lambda_adder
-from tokens import print_tokens
-
-
-def hformat(word: str, decal="/", total_width: int = 80) -> str:
-    remaining = total_width - len(word)
-    left_slashes = math.ceil(remaining / 2)
-    right_slashes = math.floor(remaining / 2)
-
-    return decal * left_slashes + word + decal * right_slashes
+assert load_dotenv(), ".env file empty or not found"
 
 
 def main():
     print("////////////////////////////// module_01 //////////////////////////////")
     print("===== RUNNABLES =====")
-    print("Add one to 7:", add_one_runnable.invoke(7))
-    print("Adding 3 and 9:", adder_runnable.invoke({"a": 3, "b": 9}))
-    print("Greeter:", greeter_runnable.invoke("World!"))
-    print(f"Lambda adder: {lambda_adder.invoke({'a': 1, 'b': 2})}")
-
-    print("===== CHAIN =====")
-    print(f"Adder chain: {adder_chain.invoke({'a': 1, 'b': 2})}")
-
-    print("===== PARALLEL CHAIN =====")
-    print(parallel_chain.get_graph().draw_ascii())
-    set_debug(True)
-    print(f"output: {parallel_chain.invoke({'a': 7, 'b': 2})}")
-    set_debug(False)
-    print("===== SEQUENCE CHAIN =====")
-    print(sequence.get_graph().draw_ascii())
-    print(sequence.invoke({"a": 2, "b": 4}))
-    print(surround_thrice.invoke(3))
-    print(parallel_double_surround.invoke({"a": 3, "b": 3}))
-    print("===== TOKENIZATION =====")
-    print(print_tokens("Hello agentic world!"))
-
-    print("===== DOCUMENT =====")
-    print(file_runnable("./resources/some_file.md"))
+    print(greet("Agentic World!"))
+    # ...
 
 
 if __name__ == "__main__":
--- module_01/runnables.py
+++ module_01/runnables.py
@@ -1,38 +1,6 @@
 from langchain_core.runnables import RunnableLambda
 
 
-def add_one(a: int) -> int:
-    return a + 1
-
-
-def adder(a: int, b: int) -> int:
-    return a + b
-
-
-def adder_wrapper(input: dict[str, int]) -> int:
-    return adder(input["a"], input["b"])
-
-
-class Greeter:
-    greeting: str
-
-    def __init__(self, greeting: str):
-        self.greeting = greeting
-
-    def __call__(self, *args) -> str:
-        invoke_input = args[
-            0
-        ]  # invoke input will be the first argument of this call function
-        return self.greeting + " " + invoke_input
-
-
-lambda_adder: RunnableLambda[dict[str, int], int] = RunnableLambda(
-    lambda d: d["a"] + d["b"]
-)
-
-add_one_runnable: RunnableLambda[int, int] = RunnableLambda(add_one)
-adder_runnable: RunnableLambda[dict[str, int], int] = RunnableLambda(
-    adder_wrapper, name="adder"
-)
-hello_greeter: Greeter = Greeter(greeting="Hello")
-greeter_runnable: RunnableLambda[str, str] = RunnableLambda(hello_greeter)
+# INFO: Sample function, imported and used in `./module_01/main.py`.
+def greet(name):
+    return "Hello " + name
