1428515d94e859866fefae641462267bbd432582
Lo, B. <39482973+lo-b@users.noreply.github.com>
2025-11-21 00:14:08 +0100

    feat: remove rest of solutions; keep starter code

--- module_04/src/workflows/basic_chatbot.py
+++ module_04/src/workflows/basic_chatbot.py
@@ -7,7 +7,6 @@ from langgraph.graph.message import add_messages
 from typing_extensions import TypedDict
 
 
-# INFO: has a 'messages' containing a list of 'BaseMessags'; enables 'chat' mode.
 class State(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
@@ -21,11 +20,6 @@ def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
 
 
-# Add the chatbot node
 graph_builder.add_node("chatbot", chatbot)
-
-# Connect START to chatbot
 graph_builder.add_edge(START, "chatbot")
-
-# Compile normally - no interrupt needed for basic chat
 basic_chatbot = graph_builder.compile()
